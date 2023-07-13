4c19e583cddcb8e2c4f9cea0258fb0f69717ad97
chan9511 <131934006+chan9511@users.noreply.github.com>
2023-07-13 17:32:44 +0900

    [level 2] Title: 소수 찾기, Time: 1.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,26 +1,36 @@
+# 순열 조합 import
 from itertools import permutations
 
-def is_prime_number(x) :
+# 소수 찾는식
+def sosu(x) :
+    # 일단 소수라는 건 자기와 1 이라도 나와야 소수자격이되는데
+    # 2보다 작으면 약수는 1밖에 없으니 탈락
     if x < 2 :
         return False
     
+    # 자기 자신과 1말고 나누어 떨어지는 수 찾는 식
+    # 만약에 있다면? 소수자격 탈락
     for i in range(2, x) :
         if x % i == 0 :
             return False
-            
+    
+    # 다 통과한 숫자 합격
     return True
 
 def solution(numbers):
     answer = 0
     nums = []
     
-    for i in range(1, len(numbers)+1) :
-        #순열 모듈 사용해서 나올 수 있는 모든 수 조합
-        nums.append(list(set(map(''.join, permutations(numbers, i)))))
-    per = list(set(map(int, set(sum(nums, [])))))
     
-    for p in per :
-        if is_prime_number(p) == True :
+    # 순열 모듈 사용해서 나올 수 있는 모든 수
+    for i in range(1, len(numbers)+1) :
+        nums.append(list(set(map(''.join, permutations(numbers, i)))))
+    
+    # 그걸 깔끔하게 가져와 
+    sunyeol = list(set(map(int, set(sum(nums, [])))))
+    
+    for j in sunyeol :
+        if sosu(j) == True :
             answer += 1
 
     return answer
