503e6f3fe8925ebf5ade93893c02f343e2a53a44
chan9511 <131934006+chan9511@users.noreply.github.com>
2023-12-01 22:40:28 +0900

    Update x만큼 간격이 있는 n개의 숫자.py

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
@@ -1,4 +1,5 @@
 # x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 만들어야 한다.
+# answer를 리스트 컴프리헨션을 사용해, x * i 값이 answer에 들어갈 수 있도록 만들어 주었다.
 def solution(x,n):
     answer = [x * i for i in range(1,n+1)]
     return answer
