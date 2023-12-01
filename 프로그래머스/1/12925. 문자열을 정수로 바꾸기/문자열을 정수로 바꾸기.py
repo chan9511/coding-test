0507c851e638c1cd2cbd80ba46a0355c88877825
chan9511 <131934006+chan9511@users.noreply.github.com>
2023-12-01 21:35:04 +0900

    Update 문자열을 정수로 바꾸기.py

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.py
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.py
@@ -1,3 +1,6 @@
+# s는 문자열이라고 주어졌고, 숫자로 변환하는 함수를 완성 하는 문제.
+# s를 숫자로 바꾼다는 것은 int형변환을 한다는 뜻.
+# answer를 s의 int형이라고 선언해주고 숫자로 변환하는 것을 리턴하였다.
 def solution(s):
-    a = int(s)
-    return a
+    answer = int(s)
+    return answer
